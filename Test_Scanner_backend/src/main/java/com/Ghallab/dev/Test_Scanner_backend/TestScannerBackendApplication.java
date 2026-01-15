5e83109aca1755af472dbf0b25240382b6bea840
ghallababdullah <abdullahghallab20@gmail.com>
2026-01-15 23:28:59 +0300

    changed from kotlin to java

---
+++ Test_Scanner_backend/src/main/java/com/Ghallab/dev/Test_Scanner_backend/TestScannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Ghallab.dev.Test_Scanner_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestScannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestScannerBackendApplication.class, args);
+	}
+
+}
