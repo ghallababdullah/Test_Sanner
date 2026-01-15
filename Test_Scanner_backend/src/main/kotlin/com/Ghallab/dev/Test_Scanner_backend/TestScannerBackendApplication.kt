d165e0b91fe6dd654e6c9f54fef3a495ba9460d1
ghallababdullah <abdullahghallab20@gmail.com>
2026-01-15 22:03:02 +0300

    first commit

---
+++ Test_Scanner_backend/src/main/kotlin/com/Ghallab/dev/Test_Scanner_backend/TestScannerBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.Ghallab.dev.Test_Scanner_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestScannerBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestScannerBackendApplication>(*args)
+}
